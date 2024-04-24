13fbe56223c01aa8be8470a414b79c985222a6dd
mmdillon38 <mmdillon@cpp.edu>
2024-04-24 10:23:02 -0700

    editing grammar in code

--- anylsis_script.R
+++ anylsis_script.R
@@ -1,4 +1,4 @@
-#this is script where we are anaylzing cows for the GIS Class project
+#this is script where we are analyzing cows for the GIS Class project
 #gonna push back and forth from github
 head(mtcars)
-#wow I should do some data anyasis
+#wow I should do some data analysis
